a1c6427d5f0991dcc1a5774d418f5334c5385283
lakshshinde001 <laxmanshinde3105@gmail.com>
2025-03-15 07:43:24 +0530

    GraphQl setup done

--- apps/api/prisma/seed.ts
+++ apps/api/prisma/seed.ts
@@ -1,65 +1,66 @@
-import { faker } from "@faker-js/faker";
+import { faker } from '@faker-js/faker';
 
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
-function generateSlug(title : string): string{
-    return title 
+function generateSlug(title: string): string {
+  return title
     .toLowerCase()
     .trim()
     .replace(/ /g, '-')
     .replace(/[^\w-]+/g, '');
 }
 
-async function main(){
-    const users = Array.from({length:10}).map(()=> ({
-        name : faker.person.fullName(),
-        email : faker.internet.email(),
-        bio : faker.lorem.sentence(),
-        avatar : faker.image.avatar()
-    }));
+async function main() {
+  const users = Array.from({ length: 10 }).map(() => ({
+    name: faker.person.fullName(),
+    email: faker.internet.email(),
+    bio: faker.lorem.sentence(),
+    avatar: faker.image.avatar(),
+  }));
 
-    await prisma.user.createMany({
-        data: users,
-    })
+  await prisma.user.createMany({
+    data: users,
+  });
 
-    const posts = Array.from({length:40}).map(()=> ({
-        title : faker.lorem.sentence(),
-        slug : generateSlug(faker.lorem.sentence()),
-        content : faker.lorem.paragraph(3),
-        thumbnail : faker.image.urlLoremFlickr(),
-        authorId : faker.number.int({min : 1, max : 10}),
-        published : true,
-    }));
+  const posts = Array.from({ length: 40 }).map(() => ({
+    title: faker.lorem.sentence(),
+    slug: generateSlug(faker.lorem.sentence()),
+    content: faker.lorem.paragraph(3),
+    thumbnail: faker.image.urlLoremFlickr(),
+    authorId: faker.number.int({ min: 1, max: 10 }),
+    published: true,
+  }));
 
-   await  Promise.all (
-        posts.map(
-            async (post) =>
-                 await prisma.post.create({
-                    data : {
-                        ...post,
-                        comments : {
-                            createMany : {
-                                data : Array.from({length:20}).map(()=> ({
-                                    content : faker.lorem.sentence(),
-                                    authorId : faker.number.int({min : 1, max : 10})
-                                })),
-                            },
-                        },
-                    },
-                 }),
-            ),
-        )
-        console.log('seeding completed !')
-    }
+  await Promise.all(
+    posts.map(
+      async (post) =>
+        await prisma.post.create({
+          data: {
+            ...post,
+            comments: {
+              createMany: {
+                data: Array.from({ length: 20 }).map(() => ({
+                  content: faker.lorem.sentence(),
+                  authorId: faker.number.int({ min: 1, max: 10 }),
+                })),
+              },
+            },
+          },
+        }),
+    ),
+  );
+  console.log('seeding completed !');
+}
 
-    main().then(()=> {
-        prisma.$disconnect();
-        process.exit(0);
-    })
-    .catch((e) => {
-        prisma.$disconnect();
-        console.error(e);
-        process.exit(1);
-    })
+main()
+  .then(() => {
+    prisma.$disconnect();
+    process.exit(0);
+  })
+  .catch((e) => {
+    prisma.$disconnect();
+    console.error(e);
+    process.exit(1);
+  });
